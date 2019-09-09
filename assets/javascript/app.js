4730a47a6d379df7bd261a34c15277561f41e6de
Shivalibhalla <shivalibhalla@gmail.com>
2019-09-08 19:17:32 -0700

    improvement in game flow

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@
 var questionNo = 0;
 
 var correctCount = 0;
-var incorrectCount =0;
+var incorrectCount = 0;
 
 var intervalId;
 
@@ -24,6 +24,7 @@ var questionObject = [{ question: "I am first Question", answer: "first", choice
 { question: "I am second Question", answer: "second", choices: ["first", "second", "third"], time: 30 },
 { question: "I am third Question", answer: "third", choices: ["first", "second", "third"], time: 40 }
 ]
+var totalQuestion = questionObject.length;
 
 
 // display first question in html
@@ -48,7 +49,6 @@ function decrement() {
         $("#timer").text(number);
         //  ...run the stop function.
         stop();
-        timerRunning = false;
     }
     $("#timer").text(number);
 }
@@ -62,53 +62,85 @@ function run() {
 
 
 function displayQuestion(qo) {
-    var correct = qo.answer;
-    // display question 
+    // var correct = qo.answer;
+
+    // Dispaly questioon number 
+    $("#qid").text(index + 1);
+    // Display Question 
     $("#question").text(qo.question);
     // Dispaly choices
     $("#label-1").text(qo.choices[0]);
     $("#label-2").text(qo.choices[1]);
     $("#label-3").text(qo.choices[2]);
     $("#timer").text(qo.time);
+    $("#message").text("");
     run();
-    return correct;
+    // return correct;
+}
+function showResults() {
+    $(".my-row-2").addClass("invisible");
+    $(".my-row-3").addClass("invisible");
+    $(".my-row-4").addClass("invisible");
+    var resultContainer = $("<div>");
+    resultContainer.append("<h2>")
+    $("#header").append(resultContainer);
 }
 
-/////--------------------
-var index=0;
-console.log(questionObject.length)
+/////--------------------Script starts here ---------------
+var index = 0;
+console.log("there are :" + questionObject.length + " questions in quiz");
 
+$("#start-game").click(function () {
+    $(".start-page").addClass("invisible");
+    $(".my-row-2").removeClass("invisible");
+    $(".my-row-3").removeClass("invisible");
+    $(".my-row-4").removeClass("invisible");
+    // displaying first question
+    displayQuestion(questionObject[index]);
+    $("input").click(function () {
+        $("input[type='radio']:checked").each(function () {
+            var idVal = $(this).attr("id");
+            chosenAnswer = $("label[for='" + idVal + "']").text();
+            console.log("i am chose: ", chosenAnswer);
+        });
 
+        if (chosenAnswer === questionObject[index].answer) {
+            console.log("correct answer");
 
-displayQuestion(questionObject[index]);
-
-
-// grab the value of selected answer
-// compare it to the right answer
-$("label").click(function () {
-    chosenAnswer = $(this).text();
-    console.log(chosenAnswer)
-
-});
-
-$("button").click(function () {
-    // console.log(questionObject[i].answer)
-    if (chosenAnswer === questionObject[index].answer) {
-        console.log("correct answer");
-        correctCount++;
-        stop();
-        index++;
-        if ( index>2){
-            showResults();
+            index = index + 1;
+            correctCount++;
+            setTimeout(function () { displayQuestion(questionObject[index]) }, 2000);
+            $("#message").removeClass("invisible");
+            $("#message").text("Correct Answer!")
+            stop();
+            if (index === totalQuestion) {
+                console.log("found last question")
+                if (chosenAnswer === questionObject[index-1].answer) {
+                    $("#message").removeClass("invisible");
+                    $("#message").text("Correct Answer!")
+                }
+                setTimeout(function(){showResults()},2000);
+            }
+        } else {
+            $("#message").text("Incorrect Answer!")
+            setTimeout(function () { displayQuestion(questionObject[index]) }, 2000);
+            console.log("Incorrect answer :(");
+            incorrectCount++
+            $("#message").removeClass("invisible");
+            $("#message").text("InCorrect Answer!")
+            stop();
+            if (index === totalQuestion) {
+                console.log("found last question")
+                if (chosenAnswer === questionObject[index-1].answer) {
+                    $("#message").removeClass("invisible");
+                    $("#message").text("Correct Answer!")
+                }
+                setTimeout(function(){showResults()},2000);
+            }
         }
-        displayQuestion(questionObject[index]);
-        
-
-    }else{
-        console.log("Incorrect answer :(");
-        incorrectCount++
-    }
+    });
 });
+
 
 
 
